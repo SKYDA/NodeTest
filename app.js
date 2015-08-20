3f9146490745ab95a84158786b4cbf2b31cc0c00
SKYDA <misitezhangpy@sina.com>
2015-08-20 14:29:29 +0800

    som

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 var express = require('express');
 var cheerio = require('cheerio');
 var superagent = require('superagent');
-
+var fs = require("fs");
 var app = express();
 app.listen(process.env.PORT || 5000);
 
 app.get('/', function (req, res, next) {
-    superagent.get('http://movie.douban.com/')
+    superagent.get('http://movie.douban.com/j/search_subjects?type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=10000&page_start=0')
         .end(function (err, sres) {
             if (err) {
                 return next(err);
             }
             var $ = cheerio.load(sres.text);
+            fs.writeFile("./file.txt", sres.text, function (error) {
+            if (error) {
+              
+            } else {
+               
+            }
+        });
             var items = [];
-            $("a.topic_title").each(function(i, element){
-                items.push({
-                    'href' : $(element).attr('href'),
-                    'title' : $(element).find("img").attr('alt'),
-                    'src':$(element).find("img").attr('src')
-                });
-            });
-            res.send(items);
+           // $(".list .item").each(function(i, element){
+           //      console.log($(this).attr("href"));
+           //      items.push({
+           //          'href' : $(element).attr('href'),
+           //          'title' : $(element).find("img").attr('alt'),
+           //          'src':$(element).find("img").attr('src')
+           //      });
+           //  });
+           //  res.send(items);
+           res.send(sres.text);
         });
 });
